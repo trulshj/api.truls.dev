7c8b0a9cd4a623778622d7da01097f413b756afa
Truls Henrik Jakobsen <thj@variant.no>
2024-10-17 09:53:42 +0200

    feat: add support for most methods on give-me

--- app/give-me/[code]/route.ts
+++ app/give-me/[code]/route.ts
@@ -1,9 +1,33 @@
-export async function GET(request: Request, {params}: {params: {code: number}}) {
-    const {code} = params
+type GiveMeRouteParams = { params: { code: number } };
 
-    if (code < 200 || code > 599) {
-        return new Response('Status code must be between 200 and 599', { status: 400 })
-    }
+function handleCode(code: number) {
+  if (code < 200 || code > 599) {
+    return new Response("Status code must be between 200 and 599", {
+      status: 400,
+    });
+  }
 
-    return new Response(`Here is your response with status ${code}`, { status: code })
+  return new Response(`Here is your response with status ${code}`, {
+    status: code,
+  });
+}
+
+export async function GET(_: Request, { params }: GiveMeRouteParams) {
+  return handleCode(params.code);
+}
+
+export async function POST(_: Request, { params }: GiveMeRouteParams) {
+  return handleCode(params.code);
+}
+
+export async function PUT(_: Request, { params }: GiveMeRouteParams) {
+  return handleCode(params.code);
+}
+
+export async function DELETE(_: Request, { params }: GiveMeRouteParams) {
+  return handleCode(params.code);
+}
+
+export async function PATCH(_: Request, { params }: GiveMeRouteParams) {
+  return handleCode(params.code);
 }
