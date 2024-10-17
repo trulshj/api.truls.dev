5baa52b0ce0dad2f02720f6955032e17fbad1af5
Truls Henrik Jakobsen <thj@variant.no>
2024-10-17 10:01:57 +0200

    I'm too foo for this bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Atkinson_Hyperlegible } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Atkinson_Hyperlegible } from "next/font/google";
 
-const atkinson = Atkinson_Hyperlegible({ weight: ["400", "700"], subsets: ["latin"] })
+const atkinson = Atkinson_Hyperlegible({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Simple API',
-  description: 'A very simple API for testing your apps',
-}
+  title: "Simple API",
+  description: "A very simple API for testing your apps",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={atkinson.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,117 @@
-import React from "react"
+import React from "react";
 
 export default function Home() {
   return (
     <div className="px-24 py-12">
-      <h1 className="text-2xl my-4">Welcome to a very simple API</h1>
+      <h1 className="text-4xl mt-4 font-semibold">
+        Welcome to a very simple API
+      </h1>
 
-      <p className="my-2 text-xl">The valid endpoints are:</p>
+      <p className="mt-2 mb-4 text-2xl">The valid endpoints are:</p>
 
       <ul className="flex flex-col gap-4">
-        <Endpoint method="GET" path="/just-say-ok">Always returns <ReturnCode code="200" />.</Endpoint>
-        <Endpoint method="POST" path="/just-say-ok">Always returns <ReturnCode code="200" />.</Endpoint>
-        <Endpoint method="POST" path="/echo">
-          Returns the request body as the response body.
-        </Endpoint>
-        <Endpoint method="GET" path="/give-me/{returnCode}">
-          Returns the given <code className="bg-slate-200">returnCode</code> as the HTTP status code.
-        </Endpoint>
-        <Endpoint method="GET" path="/dummy-array">
-          Returns an empty json array.
-        </Endpoint>
+        <EndpointGroup name="OK">
+          <Endpoint method="GET" path="/just-say-ok">
+            Always returns <ReturnCode code="200" />.
+          </Endpoint>
+
+          <Endpoint method="POST" path="/just-say-ok">
+            Always returns <ReturnCode code="200" />.
+          </Endpoint>
+        </EndpointGroup>
+
+        <EndpointGroup name="ECHO">
+          <Endpoint method="POST" path="/echo">
+            Returns the request body as the response body with a{" "}
+            <ReturnCode code="200" />
+          </Endpoint>
+        </EndpointGroup>
+
+        <EndpointGroup name="ALL THE CODES">
+          {HTTP_METHODS.map((method) => (
+            <Endpoint key={method} method={method} path="/give-me/{returnCode}">
+              Returns the given <code className="bg-slate-200">returnCode</code>{" "}
+              as the HTTP status code.
+            </Endpoint>
+          ))}
+        </EndpointGroup>
+
+        <EndpointGroup name="EMPTY ARRAY">
+          <Endpoint method="GET" path="/dummy-array">
+            Returns an empty json array.
+          </Endpoint>
+        </EndpointGroup>
       </ul>
     </div>
-  )
+  );
 }
 
-type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+const HTTP_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"] as const;
 
 type EndpointProps = {
-  method: HttpMethod
-  path: string
-  children?: React.ReactNode
-}
+  method: HttpMethod;
+  path: string;
+  children?: React.ReactNode;
+};
 
 function Endpoint({ method, path, children }: EndpointProps) {
   return (
     <li>
-      <Method method={method} />{' '}
-      <code className="bg-slate-200">
-        {path}
-      </code>{' '}
+      <Method method={method} /> <code className="bg-slate-200">{path}</code>{" "}
       {children}
     </li>
-  )
+  );
 }
 
 function Method({ method }: { method: HttpMethod }) {
   const colors = {
-    GET: 'bg-green-200',
-    POST: 'bg-blue-200',
-    PUT: 'bg-yellow-200',
-    PATCH: 'bg-yellow-200',
-    DELETE: 'bg-red-200',
-  }
+    GET: "bg-green-200",
+    POST: "bg-blue-200",
+    PUT: "bg-yellow-200",
+    PATCH: "bg-orange-200",
+    DELETE: "bg-red-200",
+  };
 
-  return <code className={colors[method]}>{method}</code>
+  return <code className={colors[method]}>{method}</code>;
 }
 
-type ReturnCodeType = '200' | '400' | '500'
+type ReturnCodeType = "200" | "400" | "500";
 
 function ReturnCode({ code }: { code: ReturnCodeType }) {
   const codes = {
-    '200': {
-      color: 'bg-green-200',
-      text: 'OK',
+    "200": {
+      color: "bg-green-200",
+      text: "OK",
     },
-    '400': {
-      color: 'bg-yellow-200',
-      text: 'Bad Request',
+    "400": {
+      color: "bg-yellow-200",
+      text: "Bad Request",
     },
-    '500': {
-      color: 'bg-red-200',
-      text: 'Internal Server Error',
+    "500": {
+      color: "bg-red-200",
+      text: "Internal Server Error",
     },
-  }
+  };
 
-  return <code className={codes[code].color}>{code} {codes[code].text}</code>
+  return (
+    <code className={codes[code].color}>
+      {code} {codes[code].text}
+    </code>
+  );
+}
+
+function EndpointGroup({
+  children,
+  name,
+}: {
+  children: React.ReactNode;
+  name?: string;
+}) {
+  return (
+    <li className="flex flex-col gap-4 border-dashed border-4 p-2">
+      {name !== undefined && <h2 className="pl-2 font-bold text-xl">{name}</h2>}
+      {children}
+    </li>
+  );
 }
